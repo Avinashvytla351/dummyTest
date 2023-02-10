3fb3166cf4ce830350878106e4ea706992c594f6
Avinash Vytlagit <avinashvytla351@gmail.com>
2023-02-10 15:13:46 +0530

    null

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const xlsx = require('xlsx')
+const xlsx = require("xlsx");
 const cors = require("cors");
 const app = express();
-const Excel = require('exceljs');
-var spread_sheet = require('spread_sheet');
+const Excel = require("exceljs");
+var spread_sheet = require("spread_sheet");
 
 app.options("*", cors());
 app.use(
@@ -23,43 +23,47 @@ app.get("/", async (req, res) => {
 });
 app.post("/", (req, res) => {
   console.log(req.body);
-  const file = xlsx.readFile('./key.xlsx');
+  const file = xlsx.readFile("./key.xlsx");
   let sheet = file.Sheets["Sheet1"];
   let data = xlsx.utils.sheet_to_json(sheet);
-  let score=0;
-  console.log(data,2324324);
-  for(var i=0;i<data.length;i++){
+  let score = 0;
+  console.log(data, 2324324);
+  for (var i = 0; i < data.length; i++) {
     var option = data[i].option;
-    console.log(option.toUpperCase() === req.body[data[i].questionNo].toUpperCase())
-    if(req.body[data[i].questionNo] && option.toUpperCase() === req.body[data[i].questionNo].toUpperCase()){
-      score+=1;
+    console.log(
+      option.toUpperCase() === req.body[data[i].questionNo].toUpperCase()
+    );
+    if (
+      req.body[data[i].questionNo] &&
+      option.toUpperCase() === req.body[data[i].questionNo].toUpperCase()
+    ) {
+      score += 1;
     }
   }
   //write
   const workbook = new Excel.Workbook();
-  workbook.xlsx.readFile('./results.xlsx')
-    .then(() => {
-      // Get the first sheet in the workbook
-      const sheet = workbook.getWorksheet(1);
+  workbook.xlsx.readFile("./result.xlsx").then(() => {
+    // Get the first sheet in the workbook
+    const sheet = workbook.getWorksheet(1);
 
-      // Get the highest row in the sheet
-      const row = sheet.rowCount + 1;
+    // Get the highest row in the sheet
+    const row = sheet.rowCount + 1;
 
-      // Add data to the new row
-      sheet.getCell(row, 1).value = req.body.RollNumber;
-      sheet.getCell(row, 2).value = score;
+    // Add data to the new row
+    sheet.getCell(row, 1).value = req.body.RollNumber;
+    sheet.getCell(row, 2).value = score;
 
-      // Save the workbook to the same file
-      workbook.xlsx.writeFile('./results.xlsx');
-      console.log("lol")
-    });
+    // Save the workbook to the same file
+    workbook.xlsx.writeFile("./result.xlsx");
+    console.log("lol");
+  });
 
   // Printing data
   console.log(score);
-  res.send("Booka le"+score);
+  res.send("Booka le" + score);
   // res.redirect("/");
 });
 
-app.listen(4000,()=> {
+app.listen(4000, () => {
   console.log("Listening on port 4000");
 });
